6455affb163ff86f815a5f9643079a993da9f3f9
Jolie Vu <aanh.trinhvu@gmail.com>
2024-07-23 20:31:53 -0700

    addstudent.html

---
+++ addstudent.js
@@ -0,0 +1,67 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const studentForm = document.getElementById('add-student');
+    
+    if (studentForm) {
+        studentForm.addEventListener('submit', addStudent);
+    };
+
+    studentTable();
+});
+
+//Function to add a new student to local storage
+function addStudent(event) {
+    event.preventDefault();
+
+    const name = document.querySelector('#student-name').value;
+    const email = document.querySelector('#student-email').value;
+    const phone = document.querySelector('#student-phone').value;
+
+    if (name && email && phone) {
+        const students = JSON.parse(localStorage.getItem('students')) || [];
+        const id = students.length > 0 ? students[students.length - 1].id + 1 : 1; 
+        students.push({ id, name, email, phone });
+        localStorage.setItem('students', JSON.stringify(students));
+        window.location.href = 'student.html'; //redirect to dashboard page
+    } else {
+        alert('Please fill out all fields');
+    }
+}
+
+//Function to populate the student table in the student.html
+function studentTable() {
+    if (document.getElementById('student-table-body')) {
+        const studentTableBody = document.getElementById('student-table-body');
+        const students = JSON.parse(localStorage.getItem('students')) || [];
+
+        students.forEach(student => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+            <td>${student.id}</tb>
+            <td>${student.name}</tb>
+            <td>${student.email}</tb>
+            <td>${student.phone}</tb>
+            <td>
+                <button onclick="updateStudent(${student.id})">Update</button>
+                <button onclick="deleteStudent(${student.id})">Delete</button>
+            </td>`;
+            studentTableBody.appendChild(row);
+        });
+    }
+}
+
+// Function to display courses
+function displayCourses(courseList) {
+    const tableBody = document.getElementById('course-table-body');
+    tableBody.innerHTML = ''; // Clear existing rows
+    courseList.forEach(course => {
+        let row = `<tr>
+            <td>${course.id}</td>
+            <td>${course.name}</td>
+            <td>
+                <button onclick="editCourse(${course.id})">Update</button>
+                <button onclick="deleteCourse(${course.id})">Delete</button>
+            </td>
+        </tr>`;
+        tableBody.innerHTML += row;
+    });
+}
